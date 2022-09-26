1ffae5d545bdacaaad9810bad33ae7a38dcd2931
Yann <metalyanni@gmail.com>
2022-09-26 10:54:33 -0500

    Desployment Descriptor and annotations V4.0 y 2.5

---
+++ S01L12 - Deployment_Descriptor/src/main/java/org/ymartinez/servlet/Demo.java
@@ -0,0 +1,40 @@
+package org.ymartinez.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo
+ * En esta versión 2.5 no tenemos una notación de URL, debido
+ * a que es gestionada por el web.xml file
+ */
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Demo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
