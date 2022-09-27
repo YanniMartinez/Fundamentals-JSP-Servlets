8b90a0dd6c5817b8510b96715c366d49e92db08f
Yann <metalyanni@gmail.com>
2022-09-27 17:57:40 -0500

    Creación y dado de alta de una Cookie en controller

---
+++ S04L02 - Read and write operation of cookie/src/main/java/org/ymartinezm/servlets/SiteController.java
@@ -0,0 +1,62 @@
+package org.ymartinezm.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SiteController
+ */
+@WebServlet("/SiteController")
+public class SiteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SiteController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("yann") && password.equals("12345")) {
+			//Invalidamos sesiones en caso de existir
+			request.getSession().invalidate();
+			
+			//Creando una nueva sesion
+			HttpSession newSession = request.getSession(true);
+			//Dando el tiempo de actividad
+			newSession.setMaxInactiveInterval(300);
+			
+			
+			//Creación de Cookie, actuan como llave:valor
+			Cookie cUsername = new Cookie("username",username);
+			//Agregando la cookie
+			response.addCookie(cUsername);
+			response.sendRedirect("memberArea.jsp");
+		}else {
+			response.sendRedirect("login.jsp");
+		}
+		
+	}
+
+}
