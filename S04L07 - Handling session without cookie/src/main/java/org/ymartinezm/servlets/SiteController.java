ae819a5c81355adc5c6c853c174d1f4c53352a9a
Yann <metalyanni@gmail.com>
2022-09-28 16:00:29 -0500

    Deshabilitando Cookies podemos tener problema - SOLUCION inicio

---
+++ S04L07 - Handling session without cookie/src/main/java/org/ymartinezm/servlets/SiteController.java
@@ -0,0 +1,85 @@
+package org.ymartinezm.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SiteController
+ */
+@WebServlet("/SiteController")
+public class SiteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SiteController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		switch (action) {
+		case "login":
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		switch (action) {
+		case "loginSubmit":
+			authenticate(request, response);
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+	
+	public void authenticate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/**Cuando no se tiene habilitado el tema de las cookies entonces
+		 * puede que tengamos algunos problemas dentro de nuestro navegadores
+		 * es por ello que una de las posibles soluciones es hacer el ENCODING 
+		 * de la URL para poder autenticarnos sin problema*/
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("yann") && password.equals("12345")) {
+			//Invalidamos sesiones en caso de existir
+			request.getSession().invalidate();
+			
+			//Creando una nueva sesion
+			HttpSession newSession = request.getSession(true);
+			//Dando el tiempo de actividad
+			newSession.setMaxInactiveInterval(300);
+			
+			//Manda el atributo
+			newSession.setAttribute("username", username);
+			
+			response.sendRedirect(request.getContextPath()+"/MemberAreaController?action=memberArea");
+		}else {
+			response.sendRedirect(request.getContextPath()+"/SiteController?action=login");
+		}
+	}
+
+}
