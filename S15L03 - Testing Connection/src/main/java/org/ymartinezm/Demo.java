0d8874f1fe4b6ea4668cccd457149f4a6c10f922
Yann <metalyanni@gmail.com>
2022-10-03 13:39:45 -0500

    Testeando conexión a la BD usando JNDI

--- S15L03 - Testing Connection/src/main/java/org/ymartinezm/Demo.java
+++ S15L03 - Testing Connection/src/main/java/org/ymartinezm/Demo.java
@@ -1,6 +1,11 @@
 package org.ymartinezm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -17,20 +22,41 @@ import javax.sql.DataSource;
 public class Demo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	//Creando objeto de DataSource
-	private DataSource dataSource;
 	//La siguiente notación nos permite accesar a un recurso externo
 	@Resource(name="jdbc/project")
+	//Creando objeto de DataSource
+	private DataSource dataSource;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out= response.getWriter();
+		
 		//Step 1: Initialize Connection Objects
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		
-		//Step 2: Create a SQL Statements String
-		
-		//Step 3: Execute SQL Query
-		
-		//Step 4: Process the result set
-		
+		try { //Obteniendo conexión desde nuestro DataSource
+			con = dataSource.getConnection();
+			
+			//Step 2: Create a SQL Statements String
+			String query = "SELECT * FROM users";
+			stmt = con.createStatement(); //Creando statement
+			
+			//Step 3: Execute SQL Query
+			rs = stmt.executeQuery(query);
+			
+			//Step 4: Process the result set
+			while(rs.next()) {
+				out.print("<br/>" + rs.getString("email"));
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
