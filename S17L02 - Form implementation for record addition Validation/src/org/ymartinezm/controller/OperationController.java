81b681cf6728f19b67b7650f5b721af5dbe71fe2
Yann <metalyanni@gmail.com>
2022-10-04 12:29:11 -0500

    Agregando métodos para agregar usuario

---
+++ S17L02 - Form implementation for record addition Validation/src/org/ymartinezm/controller/OperationController.java
@@ -0,0 +1,85 @@
+package org.ymartinezm.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.ymartinezm.entity.User;
+import org.ymartinezm.model.UsersModel;
+
+@WebServlet("/operation")
+public class OperationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Resource(name="jdbc/project")
+	private DataSource dataSource;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String page = request.getParameter("page");
+		page = page.toLowerCase();
+		
+		switch (page) {
+		case "listusers":
+			listUsers(request, response);
+			break;
+		case "adduser":
+			addUser(request, response);
+			break;
+		default:
+			errorPage(request, response);
+			break;
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String operation = request.getParameter("form");
+		operation = operation.toLowerCase();
+		switch (operation) {
+		case "adduseroperation":
+			User newUser = new User(request.getParameter("username"),request.getParameter("email"));
+			addUserOperation(newUser);
+			listUsers(request, response);
+			
+			break;
+
+		default:
+			errorPage(request, response);
+			break;
+		}
+	}
+	
+	private void addUserOperation(User newUser) {
+		new UsersModel().addUser(newUser);
+		
+	}
+
+	public void listUsers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<User> listUsers = new ArrayList<>();
+		listUsers = new UsersModel().listUsers(dataSource);
+		request.setAttribute("listUsers",listUsers);
+		request.setAttribute("title","List of Users");
+		request.getRequestDispatcher("listUser.jsp").forward(request, response);
+	}
+	
+	public void addUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("title","Add User");
+		request.getRequestDispatcher("adduser.jsp").forward(request, response);
+	}
+
+	
+	
+	public void errorPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("title","Error Page");
+		request.getRequestDispatcher("error.jsp").forward(request, response);
+	}
+}
